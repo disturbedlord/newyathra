42cfa1d4aaf8768c9e1a59beb5ed76eedffcb99f
Avinash K <44549238+disturbedlord@users.noreply.github.com>
2019-10-02 21:07:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$('.Count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
